f9ea1821752450191264985ca00873258782e4b3
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-06-22 17:39:42 -0300

    parando o timer

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,4 +1,4 @@
-import { Play } from "phosphor-react";
+import { HandPalm, Play } from "phosphor-react";
 import {
   CountdownContainer,
   FormContainer,
@@ -7,6 +7,7 @@ import {
   // eslint-disable-next-line prettier/prettier
   Separator,
   StartCountDownButton,
+  StopCountDownButton,
   TaskInput,
 } from "./styles";
 
@@ -20,7 +21,7 @@ const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "informe a tarefa"),
   minutesAmount: zod
     .number()
-    .min(5, "o ciclo precisa ser de no minimo 5 minutos")
+    .min(1, "o ciclo precisa ser de no minimo 5 minutos")
     .max(60, "o ciclo precisa ser de no máximo 60 minutos"),
 });
 
@@ -31,6 +32,8 @@ interface Cycle {
   task: string;
   minutesAmount: number;
   startDate: Date;
+  interruptedDate?: Date;
+  finishedDate?: Date;
 }
 
 export function Home() {
@@ -48,15 +51,39 @@ export function Home() {
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+
   useEffect(() => {
+    let interval: number;
+
     if (activeCycle) {
-      setInterval(() => {
-        setAmountSecondesPassed(
-          differenceInSeconds(new Date(), activeCycle.startDate)
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate
         );
+
+        if (secondsDifference >= totalSeconds) {
+          setCycles((state) =>
+            state.map((cycle) => {
+              if (cycle.id === activeCycleId) {
+                return { ...cycle, finishedDate: new Date() };
+              } else {
+                return cycle;
+              }
+            })
+          );
+          clearInterval(interval);
+        } else {
+          setAmountSecondesPassed(secondsDifference);
+        }
       }, 1000);
     }
-  }, [activeCycle]);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeCycle, totalSeconds, activeCycleId]);
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime());
@@ -71,13 +98,27 @@ export function Home() {
     // sempre que a alteração de estado depender do valor anterior...
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(id);
+    setAmountSecondesPassed(0);
     reset();
+  }
+
+  function handleInterruptCycle() {
+    setActiveCycleId(null);
+
+    setCycles(
+      cycles.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interruptedDate: new Date() };
+        } else {
+          return cycle;
+        }
+      })
+    );
   }
 
   // verificando se há um ciclo ativo
 
   // pegando o total os segundos do ciclo ativo = minutos do ciclo / 60
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 
   // pegando o tanto de segundos que já passou => se eu tiver um ciclo ativo, vai ser o total de segundos - o segundo que se passou
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
@@ -89,6 +130,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, "0");
   const seconds = String(secondsAmount).padStart(2, "0");
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [activeCycle, minutes, seconds]);
 
   const task = watch("task");
   const isSubmitDisabled = !task;
@@ -104,6 +151,7 @@ export function Home() {
             type="text"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            disabled={!!activeCycle}
           />
           <datalist id="task-suggestions">
             <option value="props" />
@@ -117,8 +165,9 @@ export function Home() {
             id="minutesAmount"
             type="number"
             placeholder="00"
+            disabled={!!activeCycle}
             step={5}
-            min={5}
+            min={1}
             max={60}
           />
           <span>minutos.</span>
@@ -132,10 +181,17 @@ export function Home() {
           <span>{seconds[1]}</span>
         </CountdownContainer>
 
-        <StartCountDownButton disabled={isSubmitDisabled} type="submit">
-          <Play size={24} />
-          Começar
-        </StartCountDownButton>
+        {activeCycle ? (
+          <StopCountDownButton onClick={handleInterruptCycle} type="button">
+            <HandPalm size={24} />
+            Interromper
+          </StopCountDownButton>
+        ) : (
+          <StartCountDownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountDownButton>
+        )}
       </form>
     </HomeContainer>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -83,7 +83,7 @@ export const Separator = styled.div`
   justify-content: center;
 `
 
-export const StartCountDownButton = styled.button`
+export const BaseCountDownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -98,14 +98,31 @@ export const StartCountDownButton = styled.button`
 
   cursor: pointer;
 
-  background: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme['gray-100']};
 
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
-  &:not(:disabled):hover {
-    background: ${(props) => props.theme['green-700']};
-  }
+  
 `
+
+export const StartCountDownButton = styled(BaseCountDownButton)`
+      background: ${(props) => props.theme['green-500']};
+     
+      
+       &:not(:disabled):hover {
+        background: ${(props) => props.theme['green-700']};
+  }
+`;
+
+
+
+export const StopCountDownButton = styled(BaseCountDownButton)`
+      background: ${(props) => props.theme['red-500']};
+     
+
+    &:not(:disabled):hover {
+      background: ${(props) => props.theme['red-700']};
+  }
+`;
